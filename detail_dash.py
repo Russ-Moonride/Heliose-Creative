5fb75c27857a7c488f08f7083daffa431147852d
Russ-Moonride <140208843+Russ-Moonride@users.noreply.github.com>
2025-02-10 14:58:41 -0500

    Update detail_dash.py

--- detail_dash.py
+++ detail_dash.py
@@ -173,7 +173,7 @@ def main():
         # Display filtered data
         st.write("### Meta Creative Detail Breakdown")
         # List of categorical variables to choose from
-        categorical_vars = ["Batch", "Medium", "Hook", "Secondary Message", "Primary Imagery Style", "Secondary Imagery Style", "Copy Style", "Aesthetic", "Concept Description", "Video Duration", "Video Audio: Voice Over", "Video Audio: BG Music", "Video Close Message"]
+        categorical_vars = ["Ad Name", "Batch", "Medium", "Hook", "Secondary Message", "Primary Imagery Style", "Secondary Imagery Style", "Copy Style", "Aesthetic", "Concept Description", "Video Duration", "Video Audio: Voice Over", "Video Audio: BG Music", "Video Close Message"]
         
         # User selects the breakdown order
         selected_vars = st.multiselect("Select breakdown order:", categorical_vars, default=["Batch", "Medium"])
